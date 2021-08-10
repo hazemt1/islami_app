eb5bc7c71132af22657bce0299c0bea2661e9e36
Alaa fathy <alaafathy416@gmail.com>
2021-08-10 22:31:46 +0200

    Separation step
    At line 17 you have to add the widgets that need to be opened when the icons are clicked

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'MyHomePage.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-
-    return MaterialApp(
-      home: Scaffold(
-        bottomNavigationBar: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/images/Rectangle.png'),
-                fit: BoxFit.cover),
-          ),
-          child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            items: [
-              BottomNavigationBarItem(
-                  icon: new Image.asset("assets/images/radio.png"),
-                  title: new Container(height: 0.0)),
-              BottomNavigationBarItem(
-                  icon: new Image.asset("assets/images/sebha.png"),
-                  title: new Container(height: 0.0)),
-              BottomNavigationBarItem(
-                  icon: new Image.asset(
-                      "assets/images/quran-quran-svgrepo-com.png"),
-                  title: new Container(height: 0.0)),
-              BottomNavigationBarItem(
-                  icon: new Image.asset("assets/images/quran.png"),
-                  title: new Container(height: 0.0)),
-            ],
-
-          ),
-
-        ),
-      ),
+      home: MyHomePage(title: 'Flutter Page'),
     );
   }
 }
