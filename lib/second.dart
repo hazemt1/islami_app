08e78e903853a865e54be0b00f35c28c97637e40
Reham Tarek <rehamtarek666666@gmail.com>
2021-08-10 02:52:55 +0200

    Added a template second page after splash screen

---
+++ lib/second.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Second Screen")),
+      body: Center(
+          child: Text(
+            "Home page",
+            textScaleFactor: 2,
+          )),
+    );
+  }
+}
